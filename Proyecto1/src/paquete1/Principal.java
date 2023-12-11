6238bcab059e1f30aaf3dfa41101008b631d8312
macair <salva@entornos.com>
2023-12-11 09:40:25 +0100

    Segunda confirmacion

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo control de versiones con Git");
+		
+		System.out.println("Escribo otra linea para un 2º commit");
 
 	}
 
